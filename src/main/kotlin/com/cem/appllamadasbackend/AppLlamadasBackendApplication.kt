160d05b6243d5bb66065a51bddbae944ba655d42
mapapicv@gmail.com <mapapicv@gmail.com>
2026-04-14 22:46:17 -0400

    Trigger redeploy: Fix 500 & Pool implementation

--- src/main/kotlin/com/cem/appllamadasbackend/AppLlamadasBackendApplication.kt
+++ src/main/kotlin/com/cem/appllamadasbackend/AppLlamadasBackendApplication.kt
@@ -3,6 +3,7 @@ package com.cem.appllamadasbackend
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+// Trigger redeploy: Hierarchical Typification & Pool Model Sync
 @SpringBootApplication
 class AppLlamadasBackendApplication
 
